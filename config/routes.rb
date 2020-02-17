0c7c14fd7ff1d685186b8f3f74a18d4aa56ef191
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-02-17 17:31:10 +0900

    half way through

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'games#new'
+  get 'games/new'
+  get 'games/score'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
